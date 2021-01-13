ea8b596e59901c1d9143469deffa196b3112e195
kjj924 <dkansk924@naver.com>
2021-01-13 22:31:10 +0900

    외부설정(application.properties)

--- src/main/java/test/demo/ApplicationRunner.java
+++ src/main/java/test/demo/ApplicationRunner.java
@@ -1,20 +1,25 @@
 package test.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
-import org.springframework.context.ApplicationEventPublisher;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationRunner implements org.springframework.boot.ApplicationRunner {
 
-    @Autowired
-    ApplicationEventPublisher applicationEventPublisher;
+    @Value("${test.projectName}")
+    String projectName;
+
+    @Value("${test.version}")
+    int version;
+
+    @Value("${test.projectNameAndVersion}")
+    String projectNameAndVersion;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Member member = new Member("Rlawowns");
-        applicationEventPublisher.publishEvent(member);
+        System.out.println(projectName);
+        System.out.println(version);
+        System.out.println(projectNameAndVersion);
     }
 }
--- src/test/java/test/demo/DemoApplicationTests.java
+++ src/test/java/test/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package test.demo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = "test.projectName=testCodeProject")
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    Environment environment;
     @Test
-    void contextLoads() {
+    void propertiesTest() {
+        Assertions.assertThat(environment.getProperty("test.projectName")).isEqualTo("testCodeProject");
     }
 
 }
