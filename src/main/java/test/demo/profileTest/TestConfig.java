522c849051df411b58a00a109efbe4bdfb494b90
kjj924 <dkansk924@naver.com>
2021-01-15 22:26:09 +0900

    profile

---
+++ src/main/java/test/demo/profileTest/TestConfig.java
@@ -0,0 +1,15 @@
+package test.demo.profileTest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+public class TestConfig {
+
+    @Bean
+    String hello(){
+        return "TestConfig";
+    }
+}
