4884e38b4793b8a970ffb2b7eb9a2eeb55c06a18
kjj924 <dkansk924@naver.com>
2021-01-21 21:03:49 +0900

    Thymeleaf

--- src/main/java/test/demo/MVC/SampleController.java
+++ src/main/java/test/demo/MVC/SampleController.java
@@ -1,26 +1,38 @@
 package test.demo.MVC;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class SampleController {
 
     @Autowired
     SampleService sampleService;
 
     @GetMapping("hello")
+
     public String helloName(){
         return sampleService.getName()+"hello";
     }
 
 
     @PostMapping("user")
+    @ResponseBody
     public User  getUser(@RequestBody User user){
         return user;
+    }
+
+
+    @GetMapping("th")
+    public String testPage(Model model){
+        model.addAttribute("name","JaeJoon");
+        model.addAttribute("age",26);
+        return "wellcome";
     }
 
 }
